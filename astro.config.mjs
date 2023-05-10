bbf082ec1e2425e5b287e0c95be69c40c5baca8e
Walid Bouladam <bouladam.walid86@gmail.com>
2023-05-10 22:03:27 +1200

    Update config to include Sanity info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-import sanity from "astro-sanity";
+import sanity from 'astro-sanity';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity()]
+  integrations: [
+    sanity({
+      projectId: 'zcbjf0lk',
+      dataset: 'production',
+      apiVersion: '2023-05-01',
+      useCdn: true,
+    }),
+  ],
 });
